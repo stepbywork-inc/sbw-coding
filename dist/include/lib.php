9c44cd7100f60b5c7494821c60bc31f98f9a2dcf
huesugi <56067045+huesugi@users.noreply.github.com>
2022-01-17 15:42:48 +0900

    aos追加

--- dist/include/lib.php
+++ dist/include/lib.php
@@ -3,7 +3,9 @@
 <script src="assets/plugins/jquery.min.js"></script>
 <script src="assets/plugins/swiper-bundle.min.js"></script>
 <script src="plugins/lightbox/lightbox.min.js"></script>
+<script src="assets/plugins/aos.js"></script>
 <script src="assets/js/main.js"></script>
 <link rel="stylesheet" href="assets/plugins/swiper-bundle.min.css" />
 <link rel="stylesheet" href="assets/plugins/lightbox/lightbox.min.css" />
+<link rel="stylesheet" href="assets/plugins/aos.css" />
 <link rel="stylesheet" href="assets/css/style.css">
