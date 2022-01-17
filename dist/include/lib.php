298242ca625c504d511e17cf0b04d726f1364617
huesugi <56067045+huesugi@users.noreply.github.com>
2022-01-17 18:48:55 +0900

    パス間違い

--- dist/include/lib.php
+++ dist/include/lib.php
@@ -2,7 +2,7 @@
 <meta name="format-detection" content="telephone=no">
 <script src="assets/plugins/jquery.min.js"></script>
 <script src="assets/plugins/swiper-bundle.min.js"></script>
-<script src="plugins/lightbox/lightbox.min.js"></script>
+<script src="assets/plugins/lightbox/lightbox.min.js"></script>
 <script src="assets/plugins/aos.js"></script>
 <script src="assets/js/main.js"></script>
 <link rel="stylesheet" href="assets/plugins/swiper-bundle.min.css" />
