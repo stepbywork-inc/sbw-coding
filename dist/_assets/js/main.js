ff95919a12d260a14dc1eb93b0b8230705cd3017
huesugi <56067045+huesugi@users.noreply.github.com>
2022-02-01 17:46:03 +0900

    aosの追加

--- dist/_assets/js/main.js
+++ dist/_assets/js/main.js
@@ -3,6 +3,12 @@ document.addEventListener('DOMContentLoaded', function () {
 	smoothScroll();
 })
 window.addEventListener('load', function () {
+	AOS.init({
+		//offset: 300,
+		// delay: 100,
+		duration: 1000,
+		once: true,
+	});
 })
 window.addEventListener('scroll', function () {
 });
