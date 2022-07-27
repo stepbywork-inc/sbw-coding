bcb842bf9a553c8cde5e545973613b7a07d1baed
huesugi <56067045+huesugi@users.noreply.github.com>
2022-07-27 18:10:54 +0900

    aosのタイミング調整

--- dist/_assets/js/main.js
+++ dist/_assets/js/main.js
@@ -1,13 +1,14 @@
 //ua判定
 document.addEventListener('DOMContentLoaded', function () {
-})
-window.addEventListener('load', function () {
 	AOS.init({
 		//offset: 300,
 		// delay: 100,
 		duration: 1000,
 		once: true,
 	});
+})
+window.addEventListener('load', function () {
+	AOS.refresh();
 })
 window.addEventListener('scroll', function () {
 });
