c2210abc8a1092a2f246518cb221383a56096023
huesugi <56067045+huesugi@users.noreply.github.com>
2023-03-14 16:33:51 +0900

    scrollの挙動をJSで

--- dist/_assets/js/main.js
+++ dist/_assets/js/main.js
@@ -12,3 +12,33 @@ window.addEventListener('load', function () {
 })
 window.addEventListener('scroll', function () {
 });
+function smoothScroll() {
+	$('[href^="#"]').click(function(){
+		var headerHeight = $('.js-hdr').outerHeight()
+		var href = $(this).attr("href");
+		var target = $(href == "#" || href == "" ? 'html' : href);
+		var position = target.offset().top - headerHeight;
+		$('body,html').stop().animate({ scrollTop: position }, 500);
+		return false;
+	});
+}
+function pagetop() {
+  $('.js-pagetop').hide();
+  $(window).on('scroll', function () {
+    if ($(this).scrollTop() > 100) {
+      $('.js-pagetop').fadeIn(300);
+    } else {
+      $('.js-pagetop').fadeOut(300);
+    }
+
+    // 指定位置で止めたい場合
+    // var scrollPosition = $(window).height() + $(window).scrollTop(); //現在地
+    // var stopPosition = $('#footer').offset().top; //止めたい位置
+
+    // if (scrollPosition >= stopPosition) {
+    //   $('.js-pagetop').addClass('is-absolute');
+    // } else {
+    //   $('.js-pagetop').removeClass('is-absolute');
+    // }
+  });
+}
